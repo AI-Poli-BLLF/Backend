d0b5d36ab2a6b980c4a15465be5a0584e3c78e86
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2020-09-25 21:29:52 +0200

    Bugfix

--- src/main/java/it/polito/ai/virtuallabs/security/dtos/UserRegistration.java
+++ src/main/java/it/polito/ai/virtuallabs/security/dtos/UserRegistration.java
@@ -1,6 +1,7 @@
 package it.polito.ai.virtuallabs.security.dtos;
 
 import lombok.Data;
+import org.checkerframework.common.value.qual.MinLen;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
@@ -15,7 +16,7 @@ public class UserRegistration {
     private String name;
     @Pattern(regexp = "[sSdD][0-9]{6}", message = "User id must be in the form s<six numbers> or d<six numbers>")
     private String userId;
-    @Min(8)
+    @MinLen(8)
     private String password;
     @Pattern(regexp = "[sSdD][0-9]{6}@(studenti\\.)?polito\\.it",
     message = "User email must be in the form <userId>@(studenti.)polito.it")
